29f78d87901a7aa5bff84f7b69e2359162aed8fb
imoisejevs <iljamoisejevs@gmail.com>
2021-01-21 17:57:43 +0200

    14

--- get_secrets.sh
+++ get_secrets.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+touch secret/env.json
+touch secret/temp_env.json
+
 declare -a secrets=("prod/db_url" "SATOSHI" "prod/simplez_keyz")
+
 for i in "${secrets[@]}"
 do
   echo "Handling secret $i"
-  aws secretsmanager get-secret-value --secret-id $i --version-stage AWSCURRENT | jq -r '.SecretString' | jq -s add - env.json > temp_env.json
-  cp temp_env.json env.json
+  aws secretsmanager get-secret-value --secret-id $i --version-stage AWSCURRENT | jq -r '.SecretString' | jq -s add - secret/env.json > secret/temp_env.json
+  cp secret/temp_env.json secret/env.json
 done
